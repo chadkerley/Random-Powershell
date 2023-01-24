566d81820a554d574be591055f2a85dcb8c1c41b
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-01-24 09:18:31 -0600

    Update Get-O365Date.ps1

--- Get-O365Date.ps1
+++ Get-O365Date.ps1
@@ -1,9 +1,10 @@
 $machines = Get-Content "C:\machines.txt"
 
 foreach ($machine in $machines) {
-    $office = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*Office 365*"}
-    if ($office) {
-        $installDate = $office.InstallDate
+    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
+    $key = $reg.OpenSubKey("SOFTWARE\\Microsoft\\SMS\\Mobile Client\\Software Distribution\\Execution History\\ABC000123")
+    if ($key) {
+        $installDate = $key.GetValue("Start Time")
         $formattedDate = (Get-Date $installDate).ToString()
         Write-Host "Office 365 was installed on $machine on $formattedDate"
     } else {
