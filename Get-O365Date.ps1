87c76a76c0882cb5d3ec1476dcbcbe346681a43b
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-01-24 09:24:51 -0600

    Update Get-O365Date.ps1

--- Get-O365Date.ps1
+++ Get-O365Date.ps1
@@ -1,8 +1,11 @@
 $machines = Get-Content "C:\machines.txt"
 
 foreach ($machine in $machines) {
+    # Enable remote PowerShell on the machine
+    Invoke-Command -ComputerName $machine -ScriptBlock { Enable-PSRemoting -Force }
+    # Open the remote registry key
     $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machine)
-    $key = $reg.OpenSubKey("SOFTWARE\\Microsoft\\SMS\\Mobile Client\\Software Distribution\\Execution History\\ABC000123")
+    $key = $reg.OpenSubKey("SOFTWARE\\Microsoft\\SMS\\Mobile Client\\Software Distribution\\Execution History\\System\\ABC00416")
     if ($key) {
         $installDate = $key.GetValue("Start Time")
         $formattedDate = (Get-Date $installDate).ToString()
