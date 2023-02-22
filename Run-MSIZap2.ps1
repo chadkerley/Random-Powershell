e7d1cbd4b06b3d6fc6749462436d1498d103242d
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-02-22 10:54:22 -0600

    Update Run-MSIZap2.ps1

--- Run-MSIZap2.ps1
+++ Run-MSIZap2.ps1
@@ -2,13 +2,13 @@
 Param (
     [Parameter(Mandatory=$True)]
     [string]$ProductName,
+
     [Parameter(Mandatory=$True)]
     [string]$Computer
 )
 
 # Write progress to console and log file
-function Write-ProgressLog($Message)
-{
+function Write-ProgressLog($Message) {
     Write-Host $Message
     Add-Content $LogFilePath $Message
 }
@@ -16,35 +16,43 @@ function Write-ProgressLog($Message)
 # Set log file path
 $LogFilePath = "\\$Computer\c$\temp\Remove-$ProductName.log"
 
-# Get 32-bit and 64-bit GUIDs using Win32_Product
-$Products = Get-WmiObject -Class Win32_Product -ComputerName $Computer | Where-Object {$_.Name -like "*$ProductName*"}
-$Guids = @()
-foreach ($Product in $Products) {
-    $Guids += $Product.IdentifyingNumber
+# Define path to tools
+$PsExec = "C:\tools\pstools\psexec.exe"
+$MSIZap = "C:\temp\MSIZap.exe"
+
+#Copy MSIZap to remote computer
+Write-Output "Copying MSIZap.exe into C:\temp on $Computer"
+Copy-Item -Path "$MSIZap" -Destination (New-Item \\$Computer\C$\Temp -Type Container -Force) -Recurse -Force
+
+Write-ProgressLog "`nFinding products on $Computer..."
+
+# Run the command on the remote machine using PsExec
+$Command = "Get-WmiObject -Class Win32_Product | Where-Object {`$_.Name -like `"`*$ProductName*`"`} | Select-Object -ExpandProperty IdentifyingNumber"
+$Guids = & $PsExec \\$Computer -s -accepteula powershell.exe -Command $Command
+
+# If no products are found, log the message and exit
+if ($Guids -eq $null) {
+    Write-ProgressLog "`nNo products found matching $ProductName on $Computer."
+    return
 }
 
 # Write found products and GUIDs to log file
 Write-ProgressLog "`nFound the following products and GUIDs:`n"
 foreach ($Guid in $Guids) {
-    $Product = ($Products | Where-Object {$_.IdentifyingNumber -eq $Guid}).Name
+    $Command = "Get-WmiObject -Class Win32_Product | Where-Object {`$_.IdentifyingNumber -eq `"$Guid`"} | Select-Object -ExpandProperty Name"
+    $Product = & $PsExec \\$Computer -s -accepteula powershell.exe -Command $Command
     Write-ProgressLog "Product: $Product"
     Write-ProgressLog "GUID: $Guid`n"
 }
 
 # Remove products with MSIZap
-if ($Guids.Count -gt 0) {
-    Write-ProgressLog "`nRemoving products..."
-    $PsExec = "\\$Computer\c$\tools\pstools\psexec.exe"
-    $MSIZap = "\\$Computer\c$\temp\MSIZap.exe"
-    foreach ($Guid in $Guids) {
-        # Remove 32-bit version of product
-        $Status = & $PsExec \\$Computer -s -accepteula $MSIZap tw! {$Guid} 2>&1
-        Write-ProgressLog "Removing 32-bit version of $ProductName with GUID ${Guid} from $Computer: $Status"
+Write-ProgressLog "`nRemoving products..."
+foreach ($Guid in $Guids) {
+    # Remove 32-bit version of product
+    $Status = & $PsExec \\$Computer -s -accepteula C:\temp\MSIZap.exe tw! {$Guid} 2>&1
+    Write-ProgressLog "Removing 32-bit version of $ProductName with GUID {$Guid} from $Computer $Status"
 
-        # Remove 64-bit version of product
-        $Status = & $PsExec -s -accepteula \\$Computer $env:windir\sysnative\MSIZap.exe tw! {$Guid} 2>&1
-        Write-ProgressLog "Removing 64-bit version of $ProductName with GUID ${Guid} from $Computer: $Status"
-    }
-} else {
-    Write-ProgressLog "`nNo products found matching $ProductName."
+    # Remove 64-bit version of product
+    $Status = & $PsExec \\$Computer -s -accepteula C:\temp\MSIZap.exe tw64! {$Guid} 2>&1
+    Write-ProgressLog "Removing 64-bit version of $ProductName with GUID {$Guid} from $Computer $Status"
 }
