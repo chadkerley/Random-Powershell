55ed33e5c7e42f2c5020bb65bb600a3b355c685a
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-02-22 11:18:57 -0600

    Update Run-MSIZap2.ps1

--- Run-MSIZap2.ps1
+++ Run-MSIZap2.ps1
@@ -16,19 +16,20 @@ function Write-ProgressLog($Message) {
 # Set log file path
 $LogFilePath = "\\$Computer\c$\temp\Remove-$ProductName.log"
 
-# Define path to tools
-$PsExec = "C:\tools\pstools\psexec.exe"
-$MSIZap = "C:\temp\MSIZap.exe"
+# Get 32-bit and 64-bit GUIDs using the registry
+$32BitKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+$64BitKey = "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 
-#Copy MSIZap to remote computer
-Write-Output "Copying MSIZap.exe into C:\temp on $Computer"
-Copy-Item -Path "$MSIZap" -Destination (New-Item \\$Computer\C$\Temp -Type Container -Force) -Recurse -Force
-
-Write-ProgressLog "`nFinding products on $Computer..."
-
-# Run the command on the remote machine using PsExec
-$Command = "Get-WmiObject -Class Win32_Product | Where-Object {`$_.Name -like `"`*$ProductName*`"`} | Select-Object -ExpandProperty IdentifyingNumber"
-$Guids = & $PsExec \\$Computer -s -accepteula powershell.exe -Command $Command
+$Guids = Get-ChildItem $32BitKey, $64BitKey | 
+    Get-ItemProperty | 
+    Where-Object { $_.DisplayName -like "*$ProductName*" } | 
+    Select-Object -ExpandProperty UninstallString | 
+    ForEach-Object {
+        # Parse the GUID from the uninstall string
+        if ($_ -match ".*\{(.*)\}.*") {
+            $matches[1]
+        }
+    }
 
 # If no products are found, log the message and exit
 if ($Guids -eq $null) {
@@ -39,9 +40,6 @@ if ($Guids -eq $null) {
 # Write found products and GUIDs to log file
 Write-ProgressLog "`nFound the following products and GUIDs:`n"
 foreach ($Guid in $Guids) {
-    $Command = "Get-WmiObject -Class Win32_Product | Where-Object {`$_.IdentifyingNumber -eq `"$Guid`"} | Select-Object -ExpandProperty Name"
-    $Product = & $PsExec \\$Computer -s -accepteula powershell.exe -Command $Command
-    Write-ProgressLog "Product: $Product"
     Write-ProgressLog "GUID: $Guid`n"
 }
 
@@ -49,10 +47,10 @@ foreach ($Guid in $Guids) {
 Write-ProgressLog "`nRemoving products..."
 foreach ($Guid in $Guids) {
     # Remove 32-bit version of product
-    $Status = & $PsExec \\$Computer -s -accepteula C:\temp\MSIZap.exe tw! {$Guid} 2>&1
-    Write-ProgressLog "Removing 32-bit version of $ProductName with GUID {$Guid} from $Computer $Status"
+    $Status = & $PsExec \\$Computer -s -accepteula C:\temp\MSIZap.exe tw! $Guid 2>&1
+    Write-ProgressLog "Removing 32-bit version of $ProductName with GUID $Guid from $Computer $Status"
 
     # Remove 64-bit version of product
-    $Status = & $PsExec \\$Computer -s -accepteula C:\temp\MSIZap.exe tw64! {$Guid} 2>&1
-    Write-ProgressLog "Removing 64-bit version of $ProductName with GUID {$Guid} from $Computer $Status"
+    $Status = & $PsExec \\$Computer -s -accepteula C:\temp\MSIZap.exe tw64! $Guid 2>&1
+    Write-ProgressLog "Removing 64-bit version of $ProductName with GUID $Guid from $Computer $Status"
 }
