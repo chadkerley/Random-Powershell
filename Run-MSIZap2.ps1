e333c63e5027f5ab22be5da10706599a8e2959ca
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-02-22 11:52:44 -0600

    Update Run-MSIZap2.ps1

--- Run-MSIZap2.ps1
+++ Run-MSIZap2.ps1
@@ -1,4 +1,5 @@
 param (
+    [Parameter(Mandatory=$true)]
     [string]$ProductName
 )
 
@@ -7,12 +8,34 @@ $source = "\\network\path\to\MSIZap.exe"
 $destination = "C:\temp\MSIZap.exe"
 Copy-Item $source $destination
 
-# Retrieve GUIDs for products with matching name
+# Retrieve GUIDs and names for products with matching name
 $products = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq $ProductName}
 $guids = $products | ForEach-Object {$_.IdentifyingNumber}
+$names = $products | ForEach-Object {$_.Name}
 
-# Run MSIZap.exe for each matching product
+# Check if any GUIDs were found
+if ($guids.Count -eq 0) {
+    Write-Output "No products found with name '$ProductName'"
+    exit
+}
+
+# Remove each matching product and store name and GUID in arrays
+$removedNames = @()
+$removedGuids = @()
 foreach ($guid in $guids) {
     $arguments = "T[WA!] {$guid}"
-    & "C:\temp\MSIZap.exe" $arguments
+    $result = & "C:\temp\MSIZap.exe" $arguments
+    $removedNames += $names[$guids.IndexOf($guid)]
+    $removedGuids += $guid
+}
+
+# Output lists of products found and removed
+Write-Output "Products found with name '$ProductName':"
+for ($i = 0; $i -lt $guids.Count; $i++) {
+    Write-Output "  Name: $($names[$i]), GUID: $($guids[$i])"
+}
+
+Write-Output "Products removed:"
+for ($i = 0; $i -lt $removedGuids.Count; $i++) {
+    Write-Output "  Name: $($removedNames[$i]), GUID: $($removedGuids[$i])"
 }
