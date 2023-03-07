dac662b352d1351d0ed5c7fec416377961b4e332
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-03-07 09:01:08 -0600

    Update Get-NewSoftware.ps1

--- Get-NewSoftware.ps1
+++ Get-NewSoftware.ps1
@@ -1,44 +1,34 @@
-$installDate = Get-Date -Format yyyyMMdd
-$uninstallKeys32 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-$uninstallKeys64 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+# Set the registry paths for the 32-bit and 64-bit uninstall locations
+$uninstallPath32 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$uninstallPath64 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
 
-# Get products installed today from 32-bit uninstall keys
-$products32 = Get-ChildItem $uninstallKeys32 | foreach {
-    if (($_.GetValue("InstallDate") -eq $installDate) -and ($_.GetValue("DisplayName") -ne $null)) {
-        $displayName = $_.GetValue("DisplayName")
-        $guid = $_.GetValue("PSChildName")
-        $version = $_.GetValue("DisplayVersion")
-        [PSCustomObject]@{
-            DisplayName = $displayName
-            GUID = $guid
-            Version = $version
-        }
-    }
-}
+# Get the current date in the format specified (e.g. 20230307)
+$date = Get-Date -Format yyyyMMdd
 
-# Get products installed today from 64-bit uninstall keys
-$products64 = Get-ChildItem $uninstallKeys64 | foreach {
-    if (($_.GetValue("InstallDate") -eq $installDate) -and ($_.GetValue("DisplayName") -ne $null)) {
-        $displayName = $_.GetValue("DisplayName")
-        $guid = $_.GetValue("PSChildName")
-        $version = $_.GetValue("DisplayVersion")
-        [PSCustomObject]@{
-            DisplayName = $displayName
-            GUID = $guid
-            Version = $version
-        }
-    }
+# Get the list of all installed software that has an InstallDate matching the current date
+$installedSoftware32 = Get-ItemProperty $uninstallPath32 | Where-Object {$_.InstallDate -eq $date}
+$installedSoftware64 = Get-ItemProperty $uninstallPath64 | Where-Object {$_.InstallDate -eq $date}
+
+# Output the list of matching products from the 32-bit registry
+Write-Output "Products installed from 32-bit registry:"
+foreach ($product in $installedSoftware32) {
+    # Get the GUID from the Uninstall string via text manipulation
+    $uninstallString = $product.UninstallString
+    $guid = $uninstallString.Substring($uninstallString.IndexOf("{"), $uninstallString.IndexOf("}") - $uninstallString.IndexOf("{") + 1)
+    Write-Output "GUID: $guid"
+    Write-Output "Display Name: $($product.DisplayName)"
+    Write-Output "Version: $($product.DisplayVersion)"
+    Write-Output ""
 }
 
-# Output results
-if ($products32.Count -gt 0) {
-    Write-Host "Products installed today from 32-bit uninstall keys:"
-    $products32 | ft -AutoSize
-}
-if ($products64.Count -gt 0) {
-    Write-Host "Products installed today from 64-bit uninstall keys:"
-    $products64 | ft -AutoSize
-}
-if (($products32.Count -eq 0) -and ($products64.Count -eq 0)) {
-    Write-Host "No products installed today."
+# Output the list of matching products from the 64-bit registry
+Write-Output "Products installed from 64-bit registry:"
+foreach ($product in $installedSoftware64) {
+    # Get the GUID from the Uninstall string via text manipulation
+    $uninstallString = $product.UninstallString
+    $guid = $uninstallString.Substring($uninstallString.IndexOf("{"), $uninstallString.IndexOf("}") - $uninstallString.IndexOf("{") + 1)
+    Write-Output "GUID: $guid"
+    Write-Output "Display Name: $($product.DisplayName)"
+    Write-Output "Version: $($product.DisplayVersion)"
+    Write-Output ""
 }
