4aca2dcdcd57b71a1fa627436540200442d35e9c
chadkerley <34281873+chadkerley@users.noreply.github.com>
2023-03-08 13:51:19 -0600

    Create Create-VMFromTemplate.ps1

---
+++ Create-VMFromTemplate.ps1
@@ -0,0 +1,24 @@
+# Connect to vSphere server using PowerCLI
+Connect-VIServer <vSphere server>
+
+# Get datastore with the most free space
+$datastore = Get-Datastore | Sort-Object FreeSpaceGB -Descending | Select-Object -First 1
+
+# Specify the name of the template to use
+$templateName = "MyTemplate"
+
+# Read a list of hostnames from a text file
+$hostnamesFile = "<path to file>"
+$hostnames = Get-Content $hostnamesFile
+
+# Loop through each hostname in the list and create a VM
+foreach ($computerName in $hostnames) {
+    # Create a new VM configuration object
+    $vmConfig = New-VM -Name $computerName -Template $templateName -Datastore $datastore.Name
+
+    # Start the VM
+    # Start-VM -VM $vmConfig
+}
+
+# Disconnect from vSphere server
+Disconnect-VIServer -Confirm:$false
